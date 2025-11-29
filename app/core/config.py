48ddce1ffb913e7a31823e6be827319e18163a43
hyj64hyj64 <44307241+hyj64hyj64@users.noreply.github.com>
2025-11-29 16:22:58 -0500

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -4,8 +4,8 @@ from typing import Optional
 class Settings(BaseSettings):
     app_name: str = "Simple HR"
     secret_key: str = "CHANGE_ME_TO_A_LONG_RANDOM_STRING"
-    database_url: str = "sqlite:///./hr_app.db"
-
+    # database_url: str = "sqlite:///./hr_app.db"
+    database_url: str = "postgresql://distrinote_hr_user:sJFBtvBldTCd4YP3TaJoLskaqQIxNqLz@dpg-d4ljgkvgi27c73cm2ovg-a.virginia-postgres.render.com/distrinote_hr"
     # 🔹 NEW: QuickBooks scaffolding config
     qb_client_id: Optional[str] = None
     qb_client_secret: Optional[str] = None
